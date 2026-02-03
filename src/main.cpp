1ffa97834bffa8061ff5c95602b2da604ced29fd
GLRammer <45434125+GLRammer@users.noreply.github.com>
2026-02-02 17:04:06 -0800

    record testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "audio.h"
+#include <string>
 
 int main(int argc, char** argv)
 {
-    
+    paData data;
+    std::string errorstr = "";
+    if(spinUp(5.0, &data,Pa_GetDefaultInputDevice(),&errorstr)){
+        SAMPLE max=0;
+        for(int i=0;i<data.recordedSamples.size();i++){
+            if(data.recordedSamples.at(i)>max)
+                max = data.recordedSamples.at(i);
+        }
+        printf("Max amp of %f",max);
+    } else{
+        printf("%s",errorstr);
+    }
     return 0;
 }
